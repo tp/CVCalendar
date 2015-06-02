f8ee503c1b0cb9efca22729214ef453222cc9752
Timm Preetz <timm@preetz.us>
2015-06-02 13:51:20 +0200

    Add project

---
+++ CVCalendar Framework/CVCalendar Framework/CVCalendar Framework.h
@@ -0,0 +1,19 @@
+//
+//  CVCalendar Framework.h
+//  CVCalendar Framework
+//
+//  Created by Timm Preetz on 2.6.2015.
+//  Copyright (c) 2015 Timm Preetz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CVCalendar Framework.
+FOUNDATION_EXPORT double CVCalendar_FrameworkVersionNumber;
+
+//! Project version string for CVCalendar Framework.
+FOUNDATION_EXPORT const unsigned char CVCalendar_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CVCalendar_Framework/PublicHeader.h>
+
+
---
+++ CVCalendarFramework/CVCalendarFramework/CVCalendarFramework.h
@@ -0,0 +1,19 @@
+//
+//  CVCalendarFramework.h
+//  CVCalendarFramework
+//
+//  Created by Timm Preetz on 2.6.2015.
+//  Copyright (c) 2015 Timm Preetz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CVCalendarFramework.
+FOUNDATION_EXPORT double CVCalendarFrameworkVersionNumber;
+
+//! Project version string for CVCalendarFramework.
+FOUNDATION_EXPORT const unsigned char CVCalendarFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CVCalendarFramework/PublicHeader.h>
+
+
